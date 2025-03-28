6290d8c86a4ec38171cece405e5ec45672a5d877
LeonardorRah <leonardo.g2049@gmail.com>
2025-03-28 11:32:07 -0300

    v1 código váriavel
    1aula

---
+++ script.js
@@ -0,0 +1,8 @@
+function armazenarValor(){
+
+    var userInput = document.getElementById("userInput").value;
+
+    console.log("O valor da variavel é: " + userInput);
+
+    document.getElementById("valorInserido").innerText = "O valor inserido é: " + userInput
+}
